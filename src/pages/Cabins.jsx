cb70832f5a64470cd14efe7df652ff984dc6e1a9
Adhiraj Bharadwaj <adhirajbharadwaj30@gmail.com>
2024-07-30 08:08:09 +0530

    installed react query and fetched cabin data

---
+++ src/pages/Cabins.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { getCabins } from "../services/apiCabins";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable";
+
+function Cabins() {
+
+  useEffect(() => {
+    getCabins().then((data) => {
+      console.log(data);
+    });
+  }, []);
+  return (
+    <>
+    <Row type="horizontal">
+      <Heading as="h1">All cabins</Heading>
+      <p>Filter/Sort</p>
+    </Row>
+    <Row>
+      <CabinTable />
+    </Row>
+    </>
+  );
+}
+
+export default Cabins;
